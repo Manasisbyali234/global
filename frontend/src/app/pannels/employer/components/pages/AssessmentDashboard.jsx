7dc6c3cd6ee1b46e5f9cf475a3dd40ee8add8321
Manasibyali124 <your_email_used_in_that_account@gmail.com>
2026-02-02 11:09:43 +0530

    Manasi changes 309

--- frontend/src/app/pannels/employer/components/pages/AssessmentDashboard.jsx
+++ frontend/src/app/pannels/employer/components/pages/AssessmentDashboard.jsx
@@ -16,15 +16,15 @@ export default function AssessmentDashboard() {
 
 	const handleCreateAssessmentClick = () => {
 		showConfirmation(
-			'• End-to-end encryption keeps your assessments secure between you and the candidates you choose.\n\n' +
-			'• Not even Tale-Global can read or copy the content\n' +
-			'• No one outside can read, copy, or share them\n' +
-			'• Questions and answers are protected\n' +
-			'• Assessment content is encrypted\n' +
-			'• Candidate responses are secure\n' +
-			'• Results and evaluations are private\n' +
-			'• All assessment data is protected\n' +
-			'• Agree to terms and conditions or not',
+			'🔐 End-to-end encryption keeps your assessments secure between you and the candidates you choose.\n\n' +
+			'👁️‍🗨️ Not even Tale-Global can read or copy the content\n' +
+			'🛡️ No one outside can read, copy, or share them\n' +
+			'❓ Questions and answers are protected\n' +
+			'📄Assessment content is encrypted\n' +
+			'🛡️ Candidate responses are secure\n' +
+			'📊 Results and evaluations are private\n' +
+			'🗄️ All assessment data is protected\n' +
+			' ✅Agree to terms and conditions or not',
 			() => setShowModal(true),
 			null,
 			'info'
