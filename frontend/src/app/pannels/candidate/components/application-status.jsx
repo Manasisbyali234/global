6617e8d4c4889fd0770620a3426e5f57ff2c9de6
Manasisbyali234 <your-email@example.com>
2026-06-20 18:29:42 +0530

    Status update 0.24

--- frontend/src/app/pannels/candidate/components/application-status.jsx
+++ frontend/src/app/pannels/candidate/components/application-status.jsx
@@ -2152,7 +2152,7 @@ function CanStatusPage() {
 				if (isExpired && assessmentResult === 'pending') {
 					return { text: 'Completed', class: 'bg-success bg-opacity-10 text-success border border-success', feedback: '' };
 				}
-				return { text: 'Rejected', class: 'bg-danger bg-opacity-10 text-danger border border-danger', feedback: '' };
+				return { text: 'No Show', class: 'bg-danger bg-opacity-10 text-danger border border-danger', feedback: '' };
 			}
 
 			if (
