4a95b9bc73ace2d0950261c99b57d48f10e14c5a
Manasibyali124 <your_email_used_in_that_account@gmail.com>
2026-06-23 17:04:46 +0530

    manasi shortlisted button renmaeing

--- frontend/src/app/pannels/employer/components/pages/ViewAnswers.jsx
+++ frontend/src/app/pannels/employer/components/pages/ViewAnswers.jsx
@@ -205,7 +205,7 @@ export default function ViewAnswers() {
       },
       null,
       'warning',
-      computedResult === 'Pass' ? { confirmText: 'Shortlisted for Next Round' } : {}
+      {}
     );
   };
 
