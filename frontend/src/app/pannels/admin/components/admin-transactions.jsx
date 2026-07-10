495f5103e8208e51bbf6d1b142c90e168348b453
Manasibyali124 <your_email_used_in_that_account@gmail.com>
2026-07-10 17:18:29 +0530

    manasi gst number

--- frontend/src/app/pannels/admin/components/admin-transactions.jsx
+++ frontend/src/app/pannels/admin/components/admin-transactions.jsx
@@ -428,7 +428,7 @@ function AdminTransactionsPage() {
                                                     <p className="mb-1 fw-bold text-dark"></p>
                                                     <p className="mb-1">Whitefield, Bengaluru, Karnataka 560066</p>
                                                     <p className="mb-1"><strong>GSTIN:</strong> 29ABCFG9123F1ZE</p>
-                                                    <p className="mb-0"><strong>Email:</strong> finance@taleglobal.com</p>
+                                                    <p className="mb-0"><strong>Email:</strong> support@taleglobal.net</p>
                                                 </div>
                                             </div>
                                             <div className="text-end">
